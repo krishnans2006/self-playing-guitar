3fc3d661209aa4fa59e9cb6956a314ce57f7ab98
Krishnan Shankar <krishnans2006@gmail.com>
2024-05-13 14:14:04 -0400

    Add temporary script to play frets with HTTP requests

---
+++ gitar/script.sh
@@ -0,0 +1,5 @@
+# curl -X GET http://127.0.0.1:5000/initialize
+curl -X GET http://127.0.0.1:5000/play/4/0/5
+curl -X GET http://127.0.0.1:5000/play/3/1/5
+curl -X GET http://127.0.0.1:5000/play/4/1/5
+sleep 5
