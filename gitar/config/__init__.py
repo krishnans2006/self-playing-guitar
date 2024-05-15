57c69264d2a4dcc11b6423012193f67f02e5b663
Krishnan Shankar <krishnans2006@gmail.com>
2024-05-14 20:23:28 -0400

    Add missing packages, add useful utilities, remove unused models

--- gitar/config/__init__.py
+++ gitar/config/__init__.py
@@ -1,3 +1,8 @@
-NOTES = {((i % 6, i // 6)): i for i in range(24)}  # (string,fret): id
+NOTES = {(i % 6, i // 6): i for i in range(24)}  # (string,fret): id
 
 GPIO = [2, 3, 4, 17, 27, 22, 10, 9, 11, 5, 6, 13, 19, 26, 14, 15, 18, 23, 24, 25, 8, 7, 12, 16, 20, 21]
+
+
+if __name__ == "__main__":
+    for (string, fret), id_ in NOTES.items():
+        print(f"ID {id_:2} | String: {string}, Fret: {fret}, GPIO Pin: {GPIO[id_]}")
--- gitar/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from song import Song
-from chord_player import ChordPlayer
-from strummer import Strummer
-from chord import Chord
-from note import Note
-
-__all__ = ("Song", "ChordPlayer", "Strummer", "Chord", "Note")
