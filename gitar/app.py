22405bba04ee473dd71465fd4b648f247e017245
Krishnan Shankar <krishnans2006@gmail.com>
2024-05-14 22:13:38 -0400

    Implement guitar dashboard with color-changing note buttons

--- gitar/app.py
+++ gitar/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, redirect, url_for, render_template
-from config import NOTES, GPIO
+from flask import Flask, render_template
+import config
 from gpiozero import DigitalOutputDevice as Pin
 
 app = Flask(__name__)
 
 # Setup pins
-frets: list[Pin] = [Pin(GPIO[id_]) for id_ in range(24)]
+frets: list[Pin] = [Pin(config.GPIO[id_]) for id_ in range(24)]
 
 for f in frets:
     f.off()
@@ -13,17 +13,17 @@ for f in frets:
 
 @app.route("/")
 def index():
-    return "Self-Playing Guitar"
+    return render_template("index.html", layout=config.LAYOUT, available_frets=config.AVAILABLE_FRETS)
 
 
 @app.route("/state")
 def state():
-    return {"state": [[frets[NOTES[string, fret]].value for fret in range(4)] for string in range(6)]}
+    return {"state": [[frets[config.NOTES[string, fret]].value for fret in range(4)] for string in range(6)]}
 
 
 @app.route("/play/<int:string>/<int:fret>/<int:time>")
 def play(string: int, fret: int, time: int):
-    pin: Pin = frets[NOTES[string, fret]]
+    pin: Pin = frets[config.NOTES[string, fret]]
     pin.blink(on_time=time, off_time=0, n=1, background=True)
     return {"result": True}
 
--- gitar/config.py
+++ gitar/config.py
@@ -2,6 +2,17 @@ NOTES = {(i % 6, i // 6): i for i in range(24)}  # (string,fret): id
 
 GPIO = [2, 3, 4, 17, 27, 22, 10, 9, 11, 5, 6, 13, 19, 26, 14, 15, 18, 23, 24, 25, 8, 7, 12, 16, 20, 21]
 
+LAYOUT = [
+    {"string": "E", "notes": ["F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E"]},
+    {"string": "B", "notes": ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]},
+    {"string": "G", "notes": ["G#", "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G"]},
+    {"string": "D", "notes": ["D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D"]},
+    {"string": "A", "notes": ["A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A"]},
+    {"string": "E", "notes": ["F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E"]},
+]
+
+AVAILABLE_FRETS = list(range(4))
+
 
 if __name__ == "__main__":
     for (string, fret), id_ in NOTES.items():
